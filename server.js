2eb5c5413f60d703a790fcd7cf8e3d59746b9b98
Langdon <lfroker@gmail.com>
2019-03-21 10:48:02 -0700

    works

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-
 const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     app = express(),
-    db = require('/.config/keys').mongoURI
-
+    db = require('./config/keys').mongoURI,
+    cors = require('cors')
 
 // serve static files in public
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({ 
-    extended: true
-  })
-);
+    bodyParser.urlencoded({ 
+      extended: true
+    })
+  );
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 app.use(cors())
 
@@ -26,7 +26,13 @@ mongoose
     .then(()=>console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-    let port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log(`BeerMe listening at port ${port}`);
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+// let port = process.env.PORT || 3000;
+
+app.listen(3000, function() {
+    console.log(`BeerMe listening at port 3000`);
 });
